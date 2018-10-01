353dff0afc0b64fb8837273cfaf3fd5feb4ce5fa
Nick Thurmes <nthurmes@gmail.com>
2018-09-30 18:26:46 -0600

    Add install script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rolling',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/the-nick-of-time/rolling',
+    license='GNU GPL v3',
+    author='Nicholas Thurmes',
+    author_email='nthurmes@gmail.com',
+    description='A module to interpret and evaluate rolls in the form that D&D gives, with some extensions.'
+)
