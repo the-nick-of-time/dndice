c000772e0e73f433fa53ae7181e303ad34b2bc90
Nick Thurmes <nthurmes@gmail.com>
2019-08-10 14:24:55 -0600

    Move tokenizer to separate function And add exceptions on a few types of failure too

--- rolling.py
+++ rolling.py
@@ -1,14 +1,14 @@
 import enum
-import string
 import typing
 
 from exceptions import InputTypeError
 from operators import OPERATORS, Operator, Side, Roll
+from tokenize_flat import tokens
 
 Value = typing.Union[Roll, int, typing.List[float]]
 Result = typing.Union[Roll, int, float]
 Final = typing.Union[int, float]
-Token = typing.Union[Value, Operator]
+Token = typing.Union[Value, Operator, typing.Literal['('], typing.Literal[')']]
 
 
 class Mode(enum.Flag):
@@ -44,86 +44,6 @@ class EvalTreeNode:
 
     def is_leaf(self) -> bool:
         return self.left is None and self.right is None
-
-
-def _string_to_operator(s: str) -> typing.Union[str, Operator]:
-    return OPERATORS.get(s, s)
-
-
-def _read_list(s: str, mode=float) -> typing.Iterable[float]:
-    """Read a list defined in a string."""
-    return list(map(mode, map(str.strip, s.split(','))))
-
-
-def tokens(s: str) -> typing.List[Token]:
-    """Splits an expression into tokens that can be parsed into an expression tree and evaluated
-
-    :param s: The expression to be parsed
-    :return: A list of tokens
-    """
-    # Every character that could be part of an operator
-    possibilities = set(''.join(OPERATORS))
-    curr_num = []
-    curr_op = []
-    tokenlist = []
-    i = 0
-    while i < len(s):
-        char = s[i]
-        if char in string.digits:
-            if curr_op:
-                op = _string_to_operator(''.join(curr_op))
-                tokenlist.append(op)
-                curr_op = []
-            curr_num.append(char)
-        elif char in possibilities or char in '()':
-            # Things that will end up on the operators stack
-            if curr_num:
-                tokenlist.append(int(''.join(curr_num)))
-                curr_num = []
-            if char == '+' and (i == 0 or s[i - 1] in possibilities or s[i - 1] == '('):
-                tokenlist.append(_string_to_operator('p'))
-                curr_op = []
-            elif char == '-' and (i == 0 or s[i - 1] in possibilities or s[i - 1] == '('):
-                tokenlist.append(_string_to_operator('m'))
-                curr_op = []
-            else:
-                if len(curr_op) == 0:
-                    # This is the first time you see an operator since last
-                    # time the list was cleared
-                    curr_op.append(char)
-                elif ''.join(curr_op + [char]) in OPERATORS:
-                    # This means that the current char is part of a
-                    # multicharacter operation like <=
-                    curr_op.append(char)
-                else:
-                    # Two separate operators; push out the old one and start
-                    # collecting the new one
-                    op = _string_to_operator(''.join(curr_op))
-                    tokenlist.append(op)
-                    curr_op = [char]
-        elif char == '[':
-            if curr_op:
-                tokenlist.append(_string_to_operator(''.join(curr_op)))
-                curr_op = []
-            # Start a list of floats
-            sidelist = []
-            i += 1
-            while s[i] != ']':
-                sidelist.append(s[i])
-                i += 1
-            tokenlist.append(_read_list(''.join(sidelist)))
-        elif char == 'F':
-            if curr_op:
-                tokenlist.append(_string_to_operator(''.join(curr_op)))
-                curr_op = []
-            # Fudge die
-            tokenlist.append([-1, 0, 1])
-        i += 1
-    if curr_num:
-        tokenlist.append(int(''.join(curr_num)))
-    elif curr_op:
-        tokenlist.append(_string_to_operator(''.join(curr_op)))
-    return tokenlist
 
 
 class EvalTree:
--- tokenize_flat.py
+++ tokenize_flat.py
@@ -1,17 +1,18 @@
 import string
 import typing
 
+from exceptions import ParseError
 from operators import OPERATORS, Operator, Roll
 
 Value = typing.Union[Roll, int, typing.List[float]]
-Token = typing.Union[Value, Operator]
+Token = typing.Union[Value, Operator, typing.Literal['('], typing.Literal[')']]
 
 
 def _string_to_operator(s: str) -> typing.Union[str, Operator]:
     return OPERATORS.get(s, s)
 
 
-def _read_list(s: str, mode=float) -> typing.Iterable[float]:
+def _read_list(s: str, mode=float) -> typing.List[float]:
     """Read a list defined in a string."""
     return list(map(mode, map(str.strip, s.split(','))))
 
@@ -19,18 +20,21 @@ def _read_list(s: str, mode=float) -> typing.Iterable[float]:
 def tokens(s: str) -> typing.List[Token]:
     """Splits an expression into tokens that can be parsed into an expression tree and evaluated
 
+    The basic algorithm is as follows:
+
     :param s: The expression to be parsed
     :return: A list of tokens
     """
     # Every character that could be part of an operator
     possibilities = set(''.join(OPERATORS))
-    curr_num = []
-    curr_op = []
-    tokenlist = []
+    nums = set(string.digits)
+    curr_num: typing.List[str] = []
+    curr_op: typing.List[str] = []
+    tokenlist: typing.List[Token] = []
     i = 0
     while i < len(s):
         char = s[i]
-        if char in string.digits:
+        if char in nums:
             if curr_op:
                 op = _string_to_operator(''.join(curr_op))
                 tokenlist.append(op)
@@ -41,10 +45,12 @@ def tokens(s: str) -> typing.List[Token]:
             if curr_num:
                 tokenlist.append(int(''.join(curr_num)))
                 curr_num = []
-            if char == '+' and (i == 0 or s[i - 1] in possibilities or s[i - 1] == '('):
+            # + and - are the unary operators iff they occur at the beginning of an expression
+            # or immediately after another operator
+            if char == '+' and (i == 0 or tokenlist[-1] in OPERATORS or tokenlist[-1] == '('):
                 tokenlist.append(_string_to_operator('p'))
                 curr_op = []
-            elif char == '-' and (i == 0 or s[i - 1] in possibilities or s[i - 1] == '('):
+            elif char == '-' and (i == 0 or tokenlist[-1] in possibilities or tokenlist[-1] == '('):
                 tokenlist.append(_string_to_operator('m'))
                 curr_op = []
             else:
@@ -67,13 +73,22 @@ def tokens(s: str) -> typing.List[Token]:
                 tokenlist.append(_string_to_operator(''.join(curr_op)))
                 curr_op = []
             # Start a list of floats
-            sidelist = []
+            sideList = []
+            begin = i
             i += 1
-            while s[i] != ']':
-                sidelist.append(s[i])
-                i += 1
-            tokenlist.append(_read_list(''.join(sidelist)))
+            try:
+                while s[i] != ']':
+                    sideList.append(s[i])
+                    i += 1
+            except IndexError:
+                raise ParseError("Unterminated die side list starting at character {start}: {slice}".format(
+                    start=begin, slice=s[i - 5 if i - 5 >= 0 else 0:i + 5 if i + 5 < len(s) else len(s)]
+                ))
+            tokenlist.append(_read_list(''.join(sideList)))
         elif char == 'F':
+            if ''.join(curr_op) not in ('d', 'da', 'dc', 'dm'):
+                raise ParseError("F is the 'fudge dice' value, and must appear as the side specifier of a roll. "
+                                 "Error at {}".format(i))
             if curr_op:
                 tokenlist.append(_string_to_operator(''.join(curr_op)))
                 curr_op = []
@@ -84,4 +99,10 @@ def tokens(s: str) -> typing.List[Token]:
         tokenlist.append(int(''.join(curr_num)))
     elif curr_op:
         tokenlist.append(_string_to_operator(''.join(curr_op)))
+    opens = tokenlist.count('(')
+    closes = tokenlist.count(')')
+    if opens > closes:
+        raise ParseError("Unclosed parentheses detected.")
+    if closes > opens:
+        raise ParseError("Unopened parentheses detected.")
     return tokenlist
