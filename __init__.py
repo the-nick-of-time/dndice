959ea4f5d0bd663cd6a6266ff3ad3e7e55a75575
Nick Thurmes <nthurmes@gmail.com>
2019-08-16 18:48:13 -0600

    Restructure for poetry packaging
    Poetry can build a package that will install the roll cli script to the path and link it correctly

--- __init__.py
+++ __init__.py
@@ -1,9 +1,1 @@
-# Access whole modules
-from .lib import operators
-from .lib import exceptions
-from .lib import evaltree
-from .lib import helpers
-from .lib import tokenizer
-
-# Hoist some core names straight into the public namespace
-from .rolling import roll, compile, tokenize, basic, verbose, Mode
+from .rolling import *
---
+++ rolling/__init__.py
@@ -0,0 +1,9 @@
+# Access whole modules
+from .lib import operators
+from .lib import exceptions
+from .lib import evaltree
+from .lib import helpers
+from .lib import tokenizer
+
+# Hoist some core names straight into the public namespace
+from .core import roll, compile, tokenize, basic, verbose, Mode
