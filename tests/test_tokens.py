6e5c36f992e309061152402e302dff5e3b9a7bed
Nick Thurmes <nthurmes@gmail.com>
2019-10-15 20:07:18 -0600

    Fix ParseError to print even when the error index is 0
    Discovered while adding tests to tokenize

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -21,7 +21,8 @@ class TokenTester(unittest.TestCase):
             "+4": [operators.OPERATORS['p'], 4],
         }
         binary = {
-            f"1{code}4": [1, op, 4] for code, op in operators.OPERATORS.items() if op.arity == operators.Side.BOTH
+            f"1{code}4": [1, op, 4] for code, op in operators.OPERATORS.items() if
+            op.arity == operators.Side.BOTH
         }
         for s, tok in itertools.chain(unary.items(), binary.items()):
             self.assertEqual(tokenizer.tokens(s), tok)
@@ -38,9 +39,11 @@ class TokenTester(unittest.TestCase):
 
     def test_precedence(self):
         results = {
-            "2*-1d4": [2, operators.OPERATORS['*'], operators.OPERATORS["m"], 1, operators.OPERATORS['d'], 4],
+            "2*-1d4": [2, operators.OPERATORS['*'], operators.OPERATORS["m"], 1,
+                       operators.OPERATORS['d'], 4],
             "2+-6": [2, operators.OPERATORS['+'], operators.OPERATORS['m'], 6],
-            "2+++6": [2, operators.OPERATORS['+'], operators.OPERATORS['p'], operators.OPERATORS['p'], 6]
+            "2+++6": [2, operators.OPERATORS['+'], operators.OPERATORS['p'],
+                      operators.OPERATORS['p'], 6]
         }
         for s, tok in results.items():
             self.assertEqual(tokenizer.tokens(s), tok)
@@ -63,6 +66,39 @@ class TokenTester(unittest.TestCase):
         for s in cases:
             with self.assertRaises(exceptions.ParseError):
                 tokenizer.tokens(s)
+
+    def test_list(self):
+        results = {
+            "1d[1,5,9]": [1, operators.OPERATORS['d'], (1, 5, 9)],
+        }
+        for s, tok in results.items():
+            self.assertEqual(tokenizer.tokens(s), tok)
+        with self.assertRaises(exceptions.ParseError):
+            tokenizer.tokens("[4,5,6]+3")
+        with self.assertRaises(exceptions.ParseError):
+            tokenizer.tokens("1d[4, 5, b]")
+
+    def test_fudge(self):
+        results = {
+            "2dF": [2, operators.OPERATORS['d'], (-1, 0, 1)]
+        }
+        for s, tok in results.items():
+            self.assertEqual(tokenizer.tokens(s), tok)
+        with self.assertRaises(exceptions.ParseError):
+            tokenizer.tokens('Fd6')
+
+    def test_parse_error(self):
+        results = {
+            "(1d4d2": "Unclosed parenthesis detected.\n    (1d4d2\n    ^",
+            "1+F": "F is the 'fudge dice' value, and must appear as the side specifier of a "
+                   "roll.\n    1+F\n      ^",
+            "1+4)": "Unopened parenthesis detected.\n    1+4)\n       ^",
+        }
+        for expr, expected in results.items():
+            try:
+                tokenizer.tokens(expr)
+            except exceptions.ParseError as e:
+                self.assertEqual(str(e), expected)
 
 
 if __name__ == '__main__':
